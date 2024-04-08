517a79f4cdbdfc68eb0a582f76f55c7c165ae30f
jeremyf <jerem.forest@gmail.com>
2024-04-08 16:57:10 -0400

    working prototype of importer

--- edullm/inputs/importer.py
+++ edullm/inputs/importer.py
@@ -1,21 +1,19 @@
 from llama_index.core import SimpleDirectoryReader
 
 class Importer():
-    def __init__(self, path:str = 'edullm/llm/rag/data'):
+    def __init__(self, path:str = 'edullm/llm/rag/data/'):
         self.path = path
         self.reader = self.llamaIndexSimpleDirectoryReader()
 
     def llamaIndexSimpleDirectoryReader(self):
-        return SimpleDirectoryReader(input_dir=self.path)
+        return SimpleDirectoryReader(input_dir=self.path, recursive=True)
 
     def load(self, workers:int = 4):
-        docs = self.reader.load_data(num_workers=workers)
-        return docs
+        data = self.reader.load_data(num_workers=workers)
+        return data 
 
 if __name__ == '__main__':
     importer = Importer()
-    print(importer.path)
-    print(importer.reader)
     documents = importer.load()
     print(len(documents))
     # print(documents)
